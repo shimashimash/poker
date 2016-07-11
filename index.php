b6d100c2a59db11adcd147c0ec75e3e7ef0cf50b
bsc.shimazu <bsc.shimazu@gmail.com>
2016-07-11 18:22:39 +0900

    hand.phpをすっきりさせてみました！

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 require_once(__DIR__ . '/poker.php');
 require_once(__DIR__ . '/hand.php');
 
-$hand = new \MyApp\Hand();
-$cards = $hand->getHand();
+$myhand = new \MyApp\Hand();
+$cards = $myhand->getHand();
 
 $poker = new \MyApp\Poker();
 $result = $poker->getYaku($cards);
@@ -15,21 +15,27 @@ $result = $poker->getYaku($cards);
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>poker</title>
+    <title>Poker</title>
     <link rel="stylesheet" href="">
 </head>
 <style>
     body {
     background: green;
     }
+    .cards {
+        float: left;
+    }
+    .cards-field {
+        overflow: hidden;
+    }
 </style>
 <body>
-    <div>
-        <?php foreach ($cards as $card): ?>
-                <img src="http://poker.host/image_trump/gif/<?php echo $card['img'].".gif"; ?>">
-                <p><?php echo $card['number']. $card['mark']; ?></p>
-        <?php endforeach; ?>
-    </div>
+    <figure class="trump">
+    <?php foreach ($cards as $card): ?>
+        <img src="http://poker.host/image_trump/gif/<?php echo $card['mark'].'_'.$card['number'].".gif"; ?>" width="50" height="50">
+        <figcaption><?php echo $card['number']. $card['mark']; ?></figcaption>
+    <?php endforeach; ?>
+    </figure>
     <div class="result">
         <p>役は<?= $result ?>です</p>
     </div>
